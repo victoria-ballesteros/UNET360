47c9ccf42b51883db59a4224d8c4821caed05c26
Victoria Ballesteros <maria.ballesteros@teamcore.cl>
2025-08-10 14:26:02 -0400

    deployments bug fix

--- unet360/src/axios.js
+++ unet360/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
 });
+
+console.log(import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api")
 
 export default api;
